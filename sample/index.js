e9e1bffbfb59a3881a0a4045d9e50e9ae2af1001
Sanjay-5406 <ssysanjay5406@gmail.com>
2026-02-20 10:16:39 +0530

    test: rest param practise

--- sample/index.js
+++ sample/index.js
@@ -1,6 +1,16 @@
-function showAnswer(ans){
-    console.log(ans)
+// function showAnswer(ans){
+//     console.log(ans)
+// }
+
+// console.log("What is the largest country in the world?")
+// setTimeout(showAnswer, 3000, "It's Russia !")
+
+// rest functions
+
+function permission(level, ...names){
+    names.forEach(
+        (names) => console.log(`Hi this is ${names} who is a ${level}`)
+    )
 }
 
-console.log("What is the largest country in the world?")
-setTimeout(showAnswer, 3000, "It's Russia !")
+permission("admin", "stark", "sam", "clark", "chris", "kent")
